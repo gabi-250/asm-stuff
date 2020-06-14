fe158432f0d1484db91b784425b90c673527b379
Gabriela Alexandra Moldovan <gabi_250@live.com>
2020-06-14 21:09:39 +0100

    Replace blanks with the guessed letter.

--- hangman/hangman.s
+++ hangman/hangman.s
@@ -16,7 +16,7 @@ hidden_char:   .byte '_'
 main:
     push %rbp
     mov %rsp, %rbp
-    sub $256, %rsp
+    sub $320, %rsp
     # read all the words from "words.txt"
     lea input_words(%rip), %rdi
     lea read_mode(%rip), %rsi
@@ -25,7 +25,7 @@ main:
     mov %rax, -8(%rbp)
     # keep track of the current line
     movq $0, -16(%rbp)
-    # the target line
+    # the target line (XXX pick a random line)
     movq $31, -24(%rbp)
     jmp .Lread_line
     leaveq
@@ -53,44 +53,52 @@ main:
     mov newline_char(%rip), %rsi
     call strchr
     movb $0, (%rax)
+    # keep track of the visible letters
+    lea -152(%rbp), %rdi
+    call strlen
+    mov %rax, -174(%rbp)
+    mov -174(%rbp), %rdi
+    movl $1, %esi
+    call calloc
+    movq %rax, -182(%rbp)
+    # make the first and last letter visible
+    movb $1, (%rax)
+    mov -174(%rbp), %rcx
+    sub $1, %rcx
+    movb $1, (%rax, %rcx, 1)
     # print the randomly selected word
 .Lprint_word:
     xor %rax, %rax
     lea word_message(%rip), %rdi
     call printf
     # print each char
-    lea -152(%rbp), %rdi
-    call strlen
-    mov %rax, %r12
+    mov -174(%rbp), %r12
+    # print the word again, but hide the middle chars
     mov $0, %r13
 1:
-    xor %rax, %rax
-    mov -152(%rbp, %r13, 1), %rsi
-    lea format_char2(%rip), %rdi
-    call printf
-    add $1, %r13
     cmp %r12, %r13
-    jne 1b
-    lea newline_str(%rip), %rdi
-    call printf
-    # print the word again, but hide the middle chars
-    mov $2, %r13
+    jnl .Ldone
+    mov -182(%rbp), %rax
+    mov (%rax, %r13, 1), %rax
+    test %al, %al
+    je .Lprint_hidden
+    jmp .Lprint_letter
+.Lprint_hidden:
     xor %rax, %rax
-    mov -152(%rbp), %rsi
     lea format_char2(%rip), %rdi
-    call printf
-1:  add $1, %r13
     mov hidden_char(%rip), %rsi
-    lea format_char2(%rip), %rdi
     call printf
-    cmp %r12, %r13
-    jne 1b
-    sub $1, %r13
-    # now print the last char:
+    jmp 2f
+.Lprint_letter:
+    xor %rax, %rax
+    lea format_char2(%rip), %rdi
     mov -152(%rbp, %r13, 1), %rsi
-    lea format_char2(%rip), %rdi
     call printf
-
+    jmp 2f
+2:
+    add $1, %r13
+    jmp 1b
+.Ldone:
     lea newline_str(%rip), %rdi
     call printf
     jmp .Lread_choice
@@ -107,4 +115,15 @@ main:
     lea -166(%rbp), %rsi
     xor %rax, %rax
     callq scanf
+    # compare the choice with the actual char
+    mov -164(%rbp), %r13
+    mov -152(%rbp, %r13, 1), %rax
+    mov -166(%rbp), %rbx
+    cmpb %al, %bl
+    jne .Lprint_word
+    # mark the letter as visible
+    # XXX mark all positions that contain this letter as visible
+    mov -182(%rbp), %rax
+    mov -164(%rbp), %r13
+    movb $1, (%rax, %r13, 1)
     jmp .Lprint_word
